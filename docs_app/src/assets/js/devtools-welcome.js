ad6f63f8a39c635e16ce74878ee861e7d4f29728
zlq4863947 <zlq4863947@gmail.com>
2020-01-01 22:06:33 +0900

    文档更新

--- docs_app/src/assets/js/devtools-welcome.js
+++ docs_app/src/assets/js/devtools-welcome.js
@@ -1,13 +1,12 @@
-var welcomeText = (
-  ' ____           _ ____      \n' +
-  '|  _ \\ __  __  | / ___|    \n' +
-  '| |_) |\\ \\/ /  | \\___ \\  \n' +
-  '|  _ <  >  < |_| |___) |    \n' +
-  '|_| \\_\\/_/\\_\\___/|____/ \n' +
-  '\nstarted experimenting with RxJS:\n' +
-  '\ntype this into the console:\n' +
-  '\nrxjs.interval(500).pipe(rxjs.operators.take(4)).subscribe(console.log)\n'
-);
+var welcomeText =
+  " ____           _ ____      \n" +
+  "|  _ \\ __  __  | / ___|    \n" +
+  "| |_) |\\ \\/ /  | \\___ \\  \n" +
+  "|  _ <  >  < |_| |___) |    \n" +
+  "|_| \\_\\/_/\\_\\___/|____/ \n" +
+  "\n开始尝试RxJS:\n" +
+  "\n在控制台中输入:\n" +
+  "\nrxjs.interval(500).pipe(rxjs.operators.take(4)).subscribe(console.log)\n";
 if (console.info) {
   console.info(welcomeText);
 } else {
